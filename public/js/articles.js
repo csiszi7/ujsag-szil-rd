539e24d4de9b936d8a2c5da9c789395900c7881a
Csiszi7 <sinko.szilard@szbiszeged.hu>
2025-09-04 14:51:46 +0200

    commit

---
+++ public/js/articles.js
@@ -0,0 +1,21 @@
+let articles = [
+    {
+        szerzo: "Szerző 1",
+        cikk: "valami",
+        "statusz": true
+    },
+    
+    {
+        szerzo: "Szerző 2",
+        cikk: "valami",
+        "statusz": true
+    },
+    
+    {
+        szerzo: "Szerző 3",
+        cikk: "valami",
+        "statusz": true
+    }
+]
+
+module.exports = articles;
---
+++ public/js/subscribers.js
@@ -0,0 +1,18 @@
+let subscribers = [
+    {
+        "nev": "Előfizető 1",
+        "statusz": true
+    },
+    
+    {
+        "nev": "Előfizető 2",
+        "statusz": false
+    },
+
+    {
+        "nev": "Előfizető 3",
+        "statusz": true
+    }
+]
+
+module.exports = subscribers;
